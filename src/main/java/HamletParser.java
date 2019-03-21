67edf148db3eefa8a490e2ade267f9e707738a43
Michael Krohn <michaelkrohn@MacBook-Pro-8.localdomain>
2019-03-21 15:58:25 -0400

    did the thing

--- src/main/java/HamletParser.java
+++ src/main/java/HamletParser.java
@@ -1,6 +1,8 @@
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by thook on 10/7/15.
@@ -8,6 +10,12 @@ import java.util.Scanner;
 public class HamletParser {
 
     private String hamletData;
+    private final Pattern HAMLET = Pattern.compile(("Hamlet"),Pattern.CASE_INSENSITIVE);
+    private final Pattern HORATIO = Pattern.compile("Horatio",Pattern.CASE_INSENSITIVE);
+    private final String LEON = "Leon";
+    private final String TARIQ = "Tariq";
+    private Matcher matcher;
+
 
     public HamletParser(){
         this.hamletData = loadFile();
@@ -35,5 +43,33 @@ public class HamletParser {
     public String getHamletData(){
         return hamletData;
     }
+
+    public String changeHoratioToTariq(String tariqString) {
+        matcher = HORATIO.matcher(tariqString);
+        tariqString = matcher.replaceAll(TARIQ);
+        return tariqString;
+    }
+
+    public String changeHamletToLeon(String hamletString) {
+        matcher = HAMLET.matcher(hamletString);
+        hamletString = matcher.replaceAll(LEON);
+        return hamletString;
+    }
+
+    public boolean findHamlet(String line){
+        return HAMLET.matcher(line).find();
+    }
+
+    public boolean findHoratio(String line){
+        return HORATIO.matcher(line).find();
+    }
+
+    public void doTheThing(){
+        String newHamlet;
+        newHamlet = changeHamletToLeon(hamletData);
+        newHamlet = changeHoratioToTariq(newHamlet);
+        System.out.println(newHamlet);
+    }
+
 
 }
--- src/test/java/HamletParserTest.java
+++ src/test/java/HamletParserTest.java
@@ -1,3 +1,4 @@
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -15,17 +16,44 @@ public class HamletParserTest {
 
     @Test
     public void testChangeHamletToLeon() {
+        String leonString = "Yo my name is Leon";
+        String hamletString = "Yo my name is Hamlet";
+
+        String actual = hamletParser.changeHamletToLeon(hamletString);
+
+        Assert.assertEquals(leonString,actual);
     }
 
     @Test
     public void testChangeHoratioToTariq() {
+        String tariqString = "Yo my name is Tariq";
+        String hamletString = "Yo my name is Hoartio";
+
+        String actual = hamletParser.changeHoratioToTariq(tariqString);
+
+        Assert.assertEquals(tariqString,actual);
     }
 
     @Test
     public void testFindHoratio() {
+        String horatioString = "Yo my name is Horatio";
+        String noHartioString = "BLah sjsiuhidnkj iauhsdid iuabisdu";
+        Assert.assertTrue(hamletParser.findHoratio(horatioString));
+        Assert.assertFalse(hamletParser.findHoratio(noHartioString));
+
     }
 
     @Test
     public void testFindHamlet() {
+        String hamletString = "Yo my name is Hamlet";
+        String noHamletString = "iuhid dibdij dibdibfiqwef udbisbsdiw edueydus isdusu hi";
+        Assert.assertTrue(hamletParser.findHamlet(hamletString));
+        Assert.assertFalse(hamletParser.findHamlet(noHamletString));
     }
+
+    @Test
+    public void doTheThingTest(){
+        hamletParser.doTheThing();
+    }
+
 }
